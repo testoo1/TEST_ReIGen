71d6873736f12cf571f981511480c4d036c660ef
Alexey Gridnev <alex-grid@mail.ru>
2017-02-19 21:30:31 +0300

    add simple ImGui window

--- main.cpp
+++ main.cpp
@@ -1,21 +1,47 @@
+#include "imgui.h"
+#include "imgui-sfml.h"
+
 #include <SFML/Graphics.hpp>
+#include <SFML/System/Clock.hpp>
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(800,600),"ReIGen");
+
+    ImGui::SFML::Init(window);
+
+    sf::Clock deltaClock;
+
+    window.resetGLStates();
 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
+            ImGui::SFML::ProcessEvent(event);
+
             if (event.type == sf::Event::Closed)
                     window.close();
         }
 
-    window.clear();
-    window.display();
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::SetNextWindowPos(ImVec2(600,0));
+        ImGui::SetNextWindowSize(ImVec2(200,600));
+        ImGui::Begin("Settings", false, ImGuiWindowFlags_NoTitleBar|
+                                        ImGuiWindowFlags_NoResize|
+                                        ImGuiWindowFlags_NoMove|
+                                        ImGuiWindowFlags_NoCollapse|
+                                        ImGuiWindowFlags_ShowBorders);
+
+        ImGui::End();
+
+        window.clear();
+        ImGui::Render();
+        window.display();
     }
 
+    ImGui::SFML::Shutdown();
     return 0;
 }
